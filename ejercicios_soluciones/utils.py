4aa2880e77d6b386d892d89e87b3a263311c9481
Fernando de la Calle Silos <fcalle@grupobme.es>
2020-06-03 14:28:31 +0200

    module requests

--- ejercicios_soluciones/utils.py
+++ ejercicios_soluciones/utils.py
@@ -1,18 +1,16 @@
 import pandas as pd 
-import urllib 
+import requests
 
 
 TOKEN = None
+BASE_URL = 'https://cloud.iexapis.com/stable'
 
-
-def get_data_iex(symbol, st_range = '5y'):
+def get_data_iex(symbol, st_range='1y'):
     if not TOKEN:
         raise Exception('Pon tu token en utils.py')
-        
-    base_url = 'https://cloud.iexapis.com/stable'
-    url = f'{base_url}/stock/{symbol}/chart/{st_range}?token={TOKEN}'
-    contents = urllib.request.urlopen(url).read()
-    data = pd.read_json(contents)
+    url = f"{BASE_URL}/stock/{symbol}/chart/{st_range}"
+    r = requests.get(url, params={'token': TOKEN}) #  'chartCloseOnly': True
+    data = pd.read_json(r.content)
     data = data[['date', 'open', 'high', 'low', 'close']]
-    data = data.set_index('date', drop=True)
+    data = data.set_index('date')
     return data
