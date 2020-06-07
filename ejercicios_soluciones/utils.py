f4d6688e0c643b935206fccf272064ee10c1f11c
fernandodelacalle <fernando.decalle@gmail.com>
2020-06-07 12:54:48 +0200

    minor mods

--- ejercicios_soluciones/utils.py
+++ ejercicios_soluciones/utils.py
@@ -5,12 +5,14 @@ import requests
 TOKEN = None
 BASE_URL = 'https://cloud.iexapis.com/stable'
 
+
 def get_data_iex(symbol, st_range='1y'):
-    if not TOKEN:
-        raise Exception('Pon tu token en utils.py')
     url = f"{BASE_URL}/stock/{symbol}/chart/{st_range}"
-    r = requests.get(url, params={'token': TOKEN}) #  'chartCloseOnly': True
+    r = requests.get(url, params={'token': TOKEN, 'chartCloseOnly': True})
     data = pd.read_json(r.content)
-    data = data[['date', 'open', 'high', 'low', 'close']]
     data = data.set_index('date')
+    data = data.close
+    data.name = symbol
     return data
+
+
